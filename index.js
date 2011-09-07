3ffcd706a9b3f4b29f6fced7b69adb2b6de25bc4
bschles <bschles@gmail.com>
2011-09-07 22:13:58 +0000

    checking from cloud9

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
  console.log(process.argv[2]);
  console.log(process.argv[3]);
  console.error('BYBY2');
+ 
+ console.log('byby');
 
  var http = require('http');
  
  http.createServer(function (req, resp) {
    resp.writeHead(200, { 'Content-Type': 'text/plain' });
-   resp.end('hellloooo node.js BYBY11!!!');
+   resp.end('hellloooo node.js BYBY22!!!');
  }).listen(process.argv[3]);
