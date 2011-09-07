698dcc455ed7a46d693dbfdc4e8dd62885608188
Benny Schlesinger <bschles@gmail.com>
2011-09-07 18:18:22 +0300

    anode.js stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+ console.log('BYBY');
+ console.log(process.argc);
+ console.log(process.argv[1]);
+ console.log(process.argv[2]);
+ console.log(process.argv[3]);
+ console.error('BYBY2');
+
+ var http = require('http');
+ 
+ http.createServer(function (req, resp) {
+   resp.writeHead(200, { 'Content-Type': 'text/plain' });
+   resp.end('hellloooo node.js BYBY!!!');
+ }).listen(process.argv[3]);
